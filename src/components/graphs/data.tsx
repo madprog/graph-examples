d1d7079526180d9af0ca164b8c7c7b2e09758818
Paul Morelle <madprog@htkc.org>
2018-03-04 21:45:21 +0100

    Move graph data to another file

---
+++ src/components/graphs/data.tsx
@@ -0,0 +1,15 @@
+import { DataItem } from './types';
+
+export const data: DataItem[] = [
+  { id: "1",  distanceToAnchor: 50,   hardness: 725 },
+  { id: "2",  distanceToAnchor: 100,  hardness: 710 },
+  { id: "3",  distanceToAnchor: 200,  hardness: 680 },
+  { id: "4",  distanceToAnchor: 300,  hardness: 640 },
+  { id: "5",  distanceToAnchor: 400,  hardness: 595 },
+  { id: "6",  distanceToAnchor: 500,  hardness: 575 },
+  { id: "7",  distanceToAnchor: 600,  hardness: 550 },
+  { id: "8",  distanceToAnchor: 700,  hardness: 500 },
+  { id: "9",  distanceToAnchor: 800,  hardness: 450 },
+  { id: "10", distanceToAnchor: 900,  hardness: 420 },
+  { id: "11", distanceToAnchor: 1000, hardness: 420 },
+];
