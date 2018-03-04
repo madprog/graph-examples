d8e7163b48afec10e5b338f3a64a0e19ed42571b
Paul Morelle <madprog@htkc.org>
2018-03-04 12:53:42 +0100

    Add a tooltip information when hovering a point

--- src/components/graphs/LineGraph.tsx
+++ src/components/graphs/LineGraph.tsx
@@ -27,11 +27,15 @@ const data: Data = [
 ];
 
 import { AxisLeft, AxisBottom } from '@vx/axis';
+import { bisector } from 'd3-array';
 import { curveNatural } from '@vx/curve';
 import { Grid } from '@vx/grid';
 import { Group } from '@vx/group';
-import { LinePath } from '@vx/shape';
+import { Line, LinePath } from '@vx/shape';
+import { localPoint } from '@vx/event';
 import { scaleLinear } from '@vx/scale';
+import { Text } from '@vx/text';
+import { Tooltip, withTooltip } from '@vx/tooltip';
 
 const height = 400;
 const width = 750;
@@ -55,8 +59,24 @@ const yScale = scaleLinear({
   domain: [300, 750],
 });
 
-class VXGraph extends React.PureComponent<GraphProps> {
+type VXGraphProps = GraphProps & {
+  handleHideToolTip: (data: Data) => ((event: any) => void),
+  handleShowToolTip: (data: Data) => ((event: any) => void),
+  tooltipData: Data,
+  tooltipLeft: number,
+  tooltipTop: number,
+};
+
+class VXGraphBase extends React.PureComponent<VXGraphProps> {
   render() {
+    const {
+      data,
+      handleHideToolTip,
+      handleShowToolTip,
+      tooltipData,
+      tooltipLeft,
+      tooltipTop,
+    } = this.props;
     return (
       <svg viewBox={`0 0 ${width} ${height}`}>
         <Group top={margin.top} left={margin.left}>
@@ -79,21 +99,87 @@ class VXGraph extends React.PureComponent<GraphProps> {
           />
           <LinePath
             curve={curveNatural}
-            data={this.props.data}
+            data={data}
             x={x} xScale={xScale}
             y={y} yScale={yScale}
           />
+          <LinePath
+            curve={curveNatural}
+            data={data}
+            x={x} xScale={xScale}
+            y={y} yScale={yScale}
+            strokeWidth={20}
+            stroke="transparent"
+            onTouchStart={handleShowToolTip}
+            onTouchMove={handleShowToolTip}
+            onMouseMove={handleShowToolTip}
+            onMouseLeave={handleHideToolTip}
+          />
+          {tooltipData && (
+            <g>
+              <Line
+                from={{ x: tooltipLeft, y: yMax }}
+                to={{ x: tooltipLeft, y: tooltipTop }}
+                stroke="rgba(92, 119, 235, 1.000)"
+                strokeWidth={2}
+                style={{pointerEvents: 'none'}}
+                strokeDasharray="2,2"
+              />
+              <Line
+                from={{ x: 0, y: tooltipTop }}
+                to={{ x: tooltipLeft, y: tooltipTop }}
+                stroke="rgba(92, 119, 235, 1.000)"
+                strokeWidth={2}
+                style={{pointerEvents: 'none'}}
+                strokeDasharray="2,2"
+              />
+              <circle
+                cx={tooltipLeft}
+                cy={tooltipTop}
+                r={4}
+                fill="rgba(92, 119, 235, 1.000)"
+                stroke="white"
+                strokeWidth={2}
+                style={{ pointerEvents: 'none' }}
+              />
+              <Text x={tooltipLeft} y={yMax}>{tooltipData[0]}</Text>
+              <Text x={0} y={tooltipTop}>{tooltipData[1]}</Text>
+            </g>
+          )}
         </Group>
       </svg>
     );
   }
 }
 
+const VXGraph = withTooltip(connect(
+  () => ({ data }),
+  (dispatch, ownProps: { hideTooltip: any, showTooltip: any }): Partial<VXGraphProps> => ({
+    handleHideToolTip: (data: Data) => (event: any) => ownProps.hideTooltip(),
+    handleShowToolTip: (data: Data) => (event: any) => {
+      const { x } = localPoint(event);
+      const x0 = xScale.invert(x - margin.left);
+      const index = bisector(d => d[0]).left(data, x0, 1);
+      const d0 = data[index - 1];
+      const d1 = data[index];
+      let d = d0;
+      if (d1 && d1[0]) {
+        d = x0 - d0[0] > d1[0] - x0 ? d1 : d0;
+      }
+      ownProps.showTooltip({
+        tooltipData: d,
+        tooltipLeft: xScale(d[0]),
+        tooltipTop: yScale(d[1]),
+      })
+    },
+  }),
+)(VXGraphBase));
+
 class GraphBase extends React.PureComponent<GraphContainerProps> {
   render() {
     switch (this.props.renderType) {
       case RenderType.VX:
-        return <VXGraph data={data} />;
+        return <VXGraph />;
       default:
         return "Line graph not implemented yet for " + RenderType[this.props.renderType];
     }
